a4db2294f810692f397f11fa5566a7e5220fa9a9
Roshanyadav2102 <94793348+Roshanyadav2102@users.noreply.github.com>
2023-11-10 20:20:00 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "Calculator",
+    subtitle: "Calculating",
+    description:
+      "Used for addition and subraction in one and multiplication and divison in other with day and night mode",
+    image: "./project-1.gif",
+    link: "#",
+  },
+  {
+    title: "Construction website",
+    subtitle: "Project",
+    description:
+      "A website for you to choose professional workers for your dream project",
+    image: "./project-2.gif",
+    link: "#",
+  },
+  {
+    title: "Age Calculator",
+    subtitle: "Count your days",
+    description: "Used to calculate age",
+    image: "./project-3.gif",
+    link: "#",
+  },
+  {
+    title: "Ticket Query",
+    subtitle: "Support system",
+    description: "Helps you to solve your problem regarding your ticket.",
+    image: "./project-4.gif",
+    link: "#",
+  },
+];
+
+export const skills = ["JavaScript", "Bootstrap", "HTML/CSS", "ReactJS"];
